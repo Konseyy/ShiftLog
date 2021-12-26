746cc1e59e9a3aaaa2f630ad3ad41e1fba45091b
Konsey <vgbukalders@gmail.com>
2021-12-26 19:49:53 +0200

    added icons to tab navigator

--- App.js
+++ App.js
@@ -4,17 +4,44 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import FooterNav from './components/NavBar';
 import {tabs} from './scenes';
-import { LogBox } from 'react-native';
+import { LogBox, Image, View } from 'react-native';
 LogBox.ignoreLogs([
   'Non-serializable values were found in the navigation state',
 ]);
 const Tab = createBottomTabNavigator();
 
 const App = () => {
+  const tabStyle = (focused) => {
+    return{
+      // borderRadius:5, 
+      // borderColor:focused?"#000000":"#ff1234", 
+      // borderWidth:focused?1:0,
+      // padding:2,
+      // backgroundColor:focused?"rgba(255,255,255,.8)":"",
+    }
+  }
   return (
     <NavigationContainer>
     <Tab.Navigator
-    screenOptions={{ headerShown: false }}
+    screenOptions={({route})=>{
+      console.log("route is", route);
+      return {
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          switch(route.name){
+            case "Shifts":
+              console.log("YESS")
+              return <View style={tabStyle(focused)}><Image style={{height:25, width:25}} source={require('./img/icons8-list-90.png')}/></View>
+            case "Data":
+              return <View style={tabStyle(focused)}><Image style={{height:25, width:25}} source={require('./img/icons8-cloud-100.png')}/></View>
+            case "Settings":
+              return <View style={tabStyle(focused)}><Image style={{height:25, width:25}} source={require('./img/icons8-settings-150.png')}/></View>
+            default:
+              return null;
+          }
+        },
+      }
+    }}
     initialRouteName="Shifts"
     backBehavior="history"
     tabBarActiveTintColor= '#000000'
