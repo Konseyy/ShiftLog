8d5a910c7d8f9956de5878535297d1328c0fe99d
Konsey <vgbukalders@gmail.com>
2021-12-31 12:48:18 +0200

    formatting config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
-};
+    useTabs: true,
+    singleQuote: true,
+    bracketSpacing: true,
+    trailingComma: "es5",
+    singleQuote: true,
+    semi: true,
+    quoteProps: "as-needed",
+    bracketLine: false,
+    arrowParens: "always",
+}
